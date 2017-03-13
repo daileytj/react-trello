55c4ce2fd2c64d2bae55d3a8e26fab879628773a
Thomas Dailey <daileytj89@gmail.com>
2017-03-13 17:21:30 -0400

    react components challenge solution

---
+++ js/components/board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './list';
+
+export default function Board() {
+    return ( <
+        div class = "board" >
+        <List / >
+        <List / >
+        <List / >
+        </div>
+    );
+}
